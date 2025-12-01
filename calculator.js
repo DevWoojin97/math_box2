96f5c139f6fc8f58b066f3199cd150ef593f9f33
DevWoojin97 <dnekdnwls@gmail.com>
2025-12-01 09:26:51 +0900

    Add multiply func

--- calculator.js
+++ calculator.js
@@ -4,6 +4,11 @@ const add = (a, b) => a + b;
 // type(a: number, b: number) -> number;
 const substract = (a, b) => a - b;
 
+// type(a: number, b: number) -> number;
+const multiply = (a, b) => {
+  return a + b;
+};
+
 // git init - 로컬 레포지토리 만들기
 // git add . - stage로
 // git commit - 로컬 레포지토리에 저장하기
