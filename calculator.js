8ef4b092bf65b6690423c14f9b3cab8505e3111b
DevWoojin97 <dnekdnwls@gmail.com>
2025-12-01 11:37:08 +0900

    update divide func

--- calculator.js
+++ calculator.js
@@ -9,6 +9,12 @@ const multiply = (a, b) => {
   return a * b;
 };
 
+// type(a:number, b:number) -> number;
+const divide = (a, b) => {
+  if (b === 0) return 0;
+  return a / b;
+};
+
 // git init - 로컬 레포지토리 만들기
 // git add . - stage로
 // git commit - 로컬 레포지토리에 저장하기
